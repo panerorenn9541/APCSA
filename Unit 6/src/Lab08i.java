2d345987db8de5f3502dab37355d723ed70b44cd
panerorenn9541 <PaneroRenN9541@sduhsd.lan>
2018-02-20 08:34:50 -0800

    Unit 6

--- Unit 6/src/Lab08i.java
+++ Unit 6/src/Lab08i.java
@@ -11,6 +11,50 @@ public class Lab08i
 {
 	public static void main ( String[] args )
 	{
+		Prime First = new Prime();
+		First.setPrime(24);
+		First.isPrime();
+		System.out.println(First.toString());
+		
+		Prime Second = new Prime();
+		Second.setPrime(7);
+		Second.isPrime();
+		System.out.println(Second.toString());
+		
+		Prime Third = new Prime();
+		Third.setPrime(100);
+		Third.isPrime();
+		System.out.println(Third.toString());
+		
+		Prime Fourth = new Prime();
+		Fourth.setPrime(113);
+		Fourth.isPrime();
+		System.out.println(Fourth.toString());
+		
+		Prime Fifth = new Prime();
+		Fifth.setPrime(65535);
+		Fifth.isPrime();
+		System.out.println(Fifth.toString());
+		
+		Prime Sixth = new Prime();
+		Sixth.setPrime(2);
+		Sixth.isPrime();
+		System.out.println(Sixth.toString());
+		
+		Prime Seventh = new Prime();
+		Seventh.setPrime(7334);
+		Seventh.isPrime();
+		System.out.println(Seventh.toString());
+		
+		Prime Eighth = new Prime();
+		Eighth.setPrime(7919);
+		Eighth.isPrime();
+		System.out.println(Eighth.toString());
+		
+		Prime Ninth = new Prime();
+		Ninth.setPrime(1115125003);
+		Ninth.isPrime();
+		System.out.println(Ninth.toString());
 		//add test cases
 		
 		
--- Unit 6/src/Prime.java
+++ Unit 6/src/Prime.java
@@ -13,27 +13,32 @@ public class Prime
 
 	public Prime()
 	{
-
-
+		number = 0;
+		setPrime(0);
 	}
 
 	public Prime(int num)
 	{
-
+		setPrime(num);
 
 	}
 
 	public void setPrime(int num)
 	{
-
+		number = num;
 
 	}
 
 	public boolean isPrime()
 	{
-
-
-
+		for (int i = 2; i<(number^(1/2)); i++)
+		{
+			if(number%i == 0)
+			{
+				return false;
+			}
+		}
+			
 
 
 
@@ -44,11 +49,16 @@ public class Prime
 	public String toString()
 	{
 		String output="";
+		
+			if (isPrime() == false)
+			{
+				output = number + " is not prime.\n";
+			}
 
-
-
-
-
+			if (isPrime() == true)
+			{
+				output = number + " is prime.\n";
+			}
 
 
 		return output;

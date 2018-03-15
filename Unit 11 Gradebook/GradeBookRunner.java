684c65cd1ac26e934d51ab8acc1eedfa02c28571
panerorenn9541 <36008213+panerorenn9541@users.noreply.github.com>
2018-03-14 22:05:15 -0700

    Add files via upload

--- Unit 11 Gradebook/GradeBookRunner.java
+++ Unit 11 Gradebook/GradeBookRunner.java
@@ -9,19 +9,40 @@ import java.util.Scanner;
 import static java.lang.System.*;
 import static java.util.Arrays.*;
 
+import java.util.ArrayList;
+
 public class GradeBookRunner
 {
    public static void main( String args[] )
    {
-		out.println("Welcome to the Class Stats program!");
+		System.out.println("Welcome to the Class Stats program!");
 		
 		Scanner keyboard = new Scanner(System.in);
-		out.print("\n\nWhat is the name of this class?");
+		System.out.print("\n\nWhat is the name of this class?");
 		String className = keyboard.nextLine();
 		System.out.print("\n\nHow Many Students are in this Class?");
 		int numStudents = keyboard.nextInt();
 		keyboard.nextLine();
-
+		Class ClassOne = new Class(className, numStudents);
+		ArrayList<Student>studentList = new ArrayList<Student>();
+		ArrayList<Class>classList = new ArrayList<Class>();
+		for(int i = 0; i<numStudents; i++)
+		{
+			System.out.print("\nWhat is the name of the student?");
+			String StudentName = keyboard.nextLine();
+			System.out.print("\nWhat are the student's grades?");
+			String StudentGrades = keyboard.nextLine();
+			Student One = new Student(StudentName, StudentGrades);
+			ClassOne.addStudent(i, One);
+		
+		}
+		System.out.println(ClassOne);
+		System.out.println("Failure List = " + ClassOne.getFailureList(70));
+		System.out.println("Highest Average = " + ClassOne.getStudentWithHighestAverage());
+		System.out.println("Lowest Average = " + ClassOne.getStudentWithLowestAverage());
+								
+		System.out.println(String.format("Class Average = %.2f",ClassOne.getClassAverage()) + "\n\n\n\n");			
+		
 
 
 

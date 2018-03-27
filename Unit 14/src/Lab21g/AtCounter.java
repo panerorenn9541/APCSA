ec837e08dc37269ab00b865c57bca87368d652e1
panerorenn9541 <36008213+panerorenn9541@users.noreply.github.com>
2018-03-26 20:06:38 -0700

    Unit 14

---
+++ Unit 14/src/Lab21g/AtCounter.java
@@ -0,0 +1,59 @@
+package Lab21g;
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class AtCounter
+{
+   private char[][] atMat;
+   private int atCount;
+
+	public AtCounter()
+	{
+		atCount=0;
+		atMat = new char[][]		{{'@','-','@','-','-','@','-','@','@','@'},
+									{'@','@','@','-','@','@','-','@','-','@'},
+									{'-','-','-','-','-','-','-','@','@','@'},
+									{'-','@','@','@','@','@','-','@','-','@'},
+									{'-','@','-','@','-','@','-','@','-','@'},
+									{'@','@','@','@','@','@','-','@','@','@'},
+									{'-','@','-','@','-','@','-','-','-','@'},
+									{'-','@','@','@','-','@','-','-','-','-'},
+									{'-','@','-','@','-','@','-','@','@','@'},
+									{'-','@','@','@','@','@','-','@','@','@'}};
+	}
+
+	public int countAts(int r, int c)
+	{
+			if(r>=0 && r<atMat.length && c>=0 && c<atMat.length && atMat[r][c] == '@')
+			{
+				atCount = atCount+1;
+				atMat[r][c] = '-';
+				countAts(r-1,c);
+				countAts(r+1,c);
+				countAts(r,c+1);
+				countAts(r,c-1);
+			}
+		//add in recursive code to count up the # of @s connected
+		//start checking at spot [r,c]
+
+
+		return 0;
+	}
+
+	public int getAtCount()
+	{
+		return atCount;
+	}
+
+	public String toString()
+	{
+		String output="";
+		output+=getAtCount()+" @s connected.";
+		return output;
+	}
+}
---
+++ Unit 14/src/Lab21g/Lab21g.java
@@ -0,0 +1,20 @@
+package Lab21g;
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class Lab21g
+{
+	public static void main(String args[])
+	{
+		//instantiate an AtCounter
+		AtCounter One = new AtCounter();
+		One.countAts(0, 0);
+		System.out.println(One.toString());
+		//test the code
+	}
+}
